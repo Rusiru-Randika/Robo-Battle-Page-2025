d39b9b207b07b4142366ef0dd169393bbd551880
Vimuth Wickramasinghe <vimuthwic3@gmail.com>
2025-03-22 02:18:39 +0530

    conatact finalized

--- src/components/ContactUs/Contact.jsx
+++ src/components/ContactUs/Contact.jsx
@@ -24,28 +24,28 @@ const teamMembers = [
   },
 ];
 
-const TeamCard = ({ member, className, index }) => (
+const TeamCard = ({ member, index }) => (
   <div
-    className={`text-white ${className} flex bg-slate-500`}
+    className={`text-white flex`}
   >
     {/* background */}
-    <div className="w-20 h-60 z-10 bg-gradient-to-b from-[#D9D9D9] via-[#62B8EE] to-[#009DFF] rounded-[48px_0px]"></div>
+    <div className="w-[146.29px] h-[263.62px] z-10 bg-gradient-to-b from-[#D9D9D9] via-[#62B8EE] to-[#009DFF] rounded-[48px_0px]"></div>
     {/* details */}
-    <div className="px-10 py-7">
+    <div className="pl-[100px] py-7">
       {/* position */}
       <div className=" flex justify-center items-center w-[161px] h-[30px] bg-[#0066FF] rounded-[32px_0px]">
-        <h3 className="text-sm font-bold ">{member.role}</h3>
+        <h3 className="font-bold ">{member.role}</h3>
       </div>
-      <div className=" inline-grid text-sm">
-        <h1 className="font-poppins font-normal">{member.name}</h1>
+      <div className=" inline-grid ">
+        <h1 className="font-poppins text-4xl">{member.name}</h1>
         {member.description && (
-          <p className="mb-2 text-sm opacity-90">{member.description}</p>
+          <p className="text-sm opacity-90">{member.description}</p>
         )}
         <div className="flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="text-blue-500 size-6">
             <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
           </svg>
-          <p className="mb-1 opacity-60 inline-flex px-4">
+          <p className="inline-flex px-4">
             {member.phone}
           </p>
         </div>
@@ -54,13 +54,13 @@ const TeamCard = ({ member, className, index }) => (
             <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
             <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
           </svg>
-          <p className="opacity-60 inline-flex px-4">
+          <p className="inline-flex px-4">
             {member.email}
           </p>
         </div>
       </div>
     </div>
-    <img src={`${fileLocation}${member.photo}`} alt={member.name} className="absolute w-60 z-20" />
+    <img src={`${fileLocation}${member.photo}`} alt={member.name} className="absolute w-80 z-20 translate-y-[-55px] translate-x-[-30px]" />
   </div>
 );
 
@@ -75,7 +75,6 @@ const Team = () => (
           key={member.name}
           member={member}
           index={index}
-          className="md:w-1/4"
         />
       ))}
     </div>
