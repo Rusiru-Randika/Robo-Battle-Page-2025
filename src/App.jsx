312a800f326f2baa3443b49b8e7a08c3affd990a
Rusiru Randika <rusirurandikakmv@gmail.com>
2025-03-21 18:28:25 +0530

    Updated Home And WinningPrizes

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Home from "./components/Home/Home";
 import Countdown from "./components/countdown";
 import Registration from "./components/Registration/Registration";
 import Awareness from "./components/AwarenessSession/Awareness";
-import Gallery from "./components/Gallery/Gallery"
+import Gallery from "./components/Gallery/Gallery";
 import About from "./components/AboutUs/About";
 import Timeline from "./components/Timeline/Timeline";
-import Sponsors from "./components/Sponsorship/Sponsors"
 import WinningPrizes from "./components/WinningPrizes/WinningPrizes";
+import Sponsors from "./components/Sponsorship/Sponsors";
 import Team from "./components/ContactUs/Contact";
 import Footer from "./components/Footer/Footer";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
@@ -22,25 +22,40 @@ const App = () => {
       <div className="min-h-screen relative overflow-hidden">
         <Navbar />
         <Routes>
-          <Route path="/" element={
-            <div >
-              <section id="home" className="pt-5"><Home /></section>
-              <Countdown />
-              <section id="register"><Registration /></section>
-              <Awareness />
-              <section id="about"><About /></section>
-              <Gallery />
-              <section className="flex justify-center m-auto" id="timeline"><Timeline /></section>
-              <Sponsors />
-              <section id="prizes"> <WinningPrizes /></section>
-              <section id="contact"><Team /></section>
-              <Footer />
-            </div>} />
+          <Route
+            path="/"
+            element={
+              <div>
+                <section id="home" className="pt-5">
+                  <Home />
+                </section>
+                <Countdown />
+                <section id="register">
+                  <Registration />
+                </section>
+                <Awareness />
+                <section id="about">
+                  <About />
+                </section>
+                <Gallery />
+                <section className="flex justify-center m-auto" id="timeline">
+                  <Timeline />
+                </section>
+                <section id="prizes">
+                  <WinningPrizes />
+                </section>
+                <Sponsors />
+                <section id="contact">
+                  <Team />
+                </section>
+                <Footer />
+              </div>
+            }
+          />
           <Route path="/register" element={<RegisterForm />} />
         </Routes>
       </div>
     </Router>
-
   );
 };
 
