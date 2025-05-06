eb8eb3a9b416079d0b09fba6dc79892151e36434
Rusiru Randika <rusirurandikakmv@gmail.com>
2025-05-07 01:09:08 +0530

    Implement code changes to enhance functionality and improve performance

--- src/components/ContactUs/Contact.jsx
+++ src/components/ContactUs/Contact.jsx
@@ -5,110 +5,118 @@ import "swiper/css";
 import "swiper/css/autoplay";
 
 // Import images
-import person1 from "../../../public/People/person1.png";
+import person1 from "../../../public/People/Person1.png";
+import person2 from "../../../public/People/Person2.png";
 
 const teamMembers = [
   {
+    role: "Senior Treasurer",
+    name: "Dr Kasun Piyumal",
+    phone: "", // Removed phone number
+    email: "Kasunp@kln.ac.lk",
+    photo: person1,
+  },
+  {
     role: "Project Manager",
     name: "Supun Tharaka",
-    phone: "+94 77 123 4567",
-    email: "Example@gmail.com",
+    phone: "+94 77 109 2426",
+    email: "tharaka-ec21028@stu.kln.ac.lk",
     photo: person1,
   },
   {
-    role: "Project Manager 2",
-    name: "Name 2",
-    phone: "+94 77 1237 4567",
-    email: "Example2@gmail.com",
+    role: "Project Manager",
+    name: "Maneesha Gunarathna",
+    phone: "+94 77 109 2426",
+    email: "gunarat-ec21026@stu.kln.ac.lk",
     photo: person1,
   },
   {
-    role: "Post",
-    name: "Name 3",
-    phone: "+94 77 123 4567",
-    email: "Example3@gmail.com",
+    role: "Project Manager",
+    name: "Kavindu Pabasara",
+    phone: "+94 71 866 9299",
+    email: "bandara-ec21031@stu.kln.ac.lk",
+    photo: person2,
+  },
+  {
+    role: "ER Coordinator",
+    name: "D.R.R.C.Dasanayake",
+    phone: "+94 70 494 9802",
+    email: "dasanay-ec21074@stu.kln.ac.lk",
     photo: person1,
   },
   {
-    role: "Post",
-    name: "Name 4",
-    phone: "+94 77 123 4567",
-    email: "Example4@gmail.com",
+    role: "ER Coordinator",
+    name: "A.P.V.P Rajarathne ",
+    phone: "+94 71 076 9883",
+    email: "rajarat-ec21009@stu.kln.ac.lk ",
     photo: person1,
   },
 ];
 
 const TeamCard = ({ member, index }) => (
-  
-
   <div className="mt-40 text-white py-6 flex flex-col md:flex-row items-center md:items-start gap-8 font-bebasneue">
+    {/* Image and Background */}
+    <div className="static min-w-[250px] min-h-[300px]">
+      {/* Background shape */}
+      <div className="absolute  md:left-0   -translate-y-[20px] w-[146.29px] h-[263.62px] z-10 bg-gradient-to-b from-[#D9D9D9] via-[#62B8EE] to-[#009DFF] rounded-[48px_0px]"></div>
 
-  {/* Image and Background */}
-  <div className="static min-w-[250px] min-h-[300px]">
-    {/* Background shape */}
-    <div className="absolute  md:left-0   -translate-y-[20px] w-[146.29px] h-[263.62px] z-10 bg-gradient-to-b from-[#D9D9D9] via-[#62B8EE] to-[#009DFF] rounded-[48px_0px]"></div>
-
-    {/* Member Photo */}
-    <img
-      src={member.photo}
-      alt={member.name}
-      className="absolute md:left-0 w-80 z-20 -translate-y-[70px] -translate-x-[30px]"
-    />
-  </div>
-
-  {/* Content */}
-  <div className="flex flex-col justify-center md:items-start items-center gap-6 pl-0  max-w-[90vw]">
-
-    {/* Position Badge */}
-    <div className="flex justify-center items-center w-[161px] h-[30px] bg-[#0066FF] rounded-[32px_0px]">
-      <h3 className="font-bold">{member.role}</h3>
+      {/* Member Photo */}
+      <img
+        src={member.photo}
+        alt={member.name}
+        className="absolute md:left-0 w-80 z-20 -translate-y-[70px] -translate-x-[30px]"
+      />
     </div>
 
-    {/* Name and Contact Info */}
-    <div className="flex flex-col items-center md:items-start text-center md:text-left gap-4">
-
-      {/* Name */}
-      <h1 className="text-2xl md:text-4xl">{member.name}</h1>
-
-      {/* Phone */}
-      <div className="flex items-center gap-2">
-        {/* Phone Icon */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="text-blue-500 size-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <p className="px-2">{member.phone}</p>
+    {/* Content */}
+    <div className="flex flex-col justify-center md:items-start items-center gap-6 pl-0  max-w-[90vw]">
+      {/* Position Badge */}
+      <div className="flex justify-center items-center w-[161px] h-[30px] bg-[#0066FF] rounded-[32px_0px]">
+        <h3 className="font-bold">{member.role}</h3>
       </div>
 
-      {/* Email */}
-      <div className="flex items-center gap-2">
-        {/* Email Icon */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="text-blue-500 size-6"
-        >
-          <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-          <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-        </svg>
-        <p className="px-2">{member.email}</p>
-      </div>
+      {/* Name and Contact Info */}
+      <div className="flex flex-col items-center md:items-start text-center md:text-left gap-4">
+        {/* Name */}
+        <h1 className="text-2xl md:text-4xl">{member.name}</h1>
 
+        {/* Phone */}
+        {member.phone && ( // Only render if phone number exists
+          <div className="flex items-center gap-2">
+            {/* Phone Icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="text-blue-500 size-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <p className="px-2">{member.phone}</p>
+          </div>
+        )}
+
+        {/* Email */}
+        <div className="flex items-center gap-2">
+          {/* Email Icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="text-blue-500 size-6"
+          >
+            <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+            <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+          </svg>
+          <p className="px-2">{member.email}</p>
+        </div>
+      </div>
     </div>
   </div>
-
-</div>
-
-
 );
 
 const Team = () => (
