7ef97377eeae974d729de2f901d5bd52b4347e83
Rusiru Randika <rusirurandikakmv@gmail.com>
2025-05-10 14:33:21 +0530

    Update logo import in Navbar component from SVG to PNG format

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FiMenu, FiX } from "react-icons/fi";
-import logo from "../../../public/Branding/logo.svg"; // Adjust path as needed
+import logo from "../../../public/Branding/Home-Logo.png"; // Adjust path as needed
 
 const Navbar = ({ registerButtonRef }) => {
   const [isOpen, setIsOpen] = useState(false);
